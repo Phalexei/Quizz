69a2bac493b86f5806b77c57f399f151cc491648
Gael Ribes <ribesg@yahoo.fr>
2014-10-13 15:30:10 +0200

    Maven

---
+++ src/main/java/imag/quizz/Main.java
@@ -0,0 +1,14 @@
+package com.github.phalexei.cloudquizz;
+
+/**
+ *
+ */
+public class Main {
+
+    /**
+     * @param args commandline arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello wolrd!");
+    }
+}
